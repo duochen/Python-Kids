3b0b6602dbb4864e80d273260651bb876910ea91
Duo Chen <duochen@yahoo.com>
2019-06-16 10:43:18 -0500

    check in code

--- Lecture01/Exercises/NiceHexSpiral.py
+++ Lecture01/Exercises/NiceHexSpiral.py
@@ -1,10 +1,10 @@
-#NiceHexSpiral.py
+# NiceHexSpiral.py
 import turtle
 
-colors=['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
 turtle.bgcolor("black")
 
-t =turtle.Pen()
+t = turtle.Pen()
 for x in range(360):
     t.pencolor(colors[x%6])
     t.width(x/100+1)
