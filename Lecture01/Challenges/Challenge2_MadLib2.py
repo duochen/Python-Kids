34581ec74da9ab73cf7d487c9e6e25bf595563c1
Duo Chen <duochen@yahoo.com>
2019-06-19 07:33:23 -0500

    check in code

--- Lecture01/Challenges/Challenge2_MadLib2.py
+++ Lecture01/Challenges/Challenge2_MadLib2.py
@@ -4,4 +4,3 @@ verb = input('Please enter a verb ending in -ed: ')
 animal = input('Please enter an animal: ')
 print('Your MadLib:')
 print('The', adjective, noun, verb, 'over the lazy brown', animal + '.')
-
