b1ced3b2bef5d6c726f51707e416b8d08a769a00
Duo Chen <duochen@yahoo.com>
2019-07-04 09:59:23 -0500

    check in code

---
+++ Lecture07/Exercises/Addition.py
@@ -0,0 +1,8 @@
+def addition(a, b):
+    c = a + b
+    return c
+
+num1 = int(input("Enter the value of a to add: "))
+num2 = int(input("'Enter the value of b to add: "))
+result = addition(num1, num2)
+print("The addition of a and b is: ", result)
