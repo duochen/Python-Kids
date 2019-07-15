4ae681021a3423a49a3e87e21dbb0e27e0774ea4
Duo Chen <duochen@yahoo.com>
2019-07-15 20:29:33 +0200

    check in code

--- Lecture04_List_And_Loops/Exercises/names.py
+++ Lecture04_List_And_Loops/Exercises/names.py
@@ -1,5 +1,13 @@
+import turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
 names = ["Donald Trump", "Steve Jobs", "Bill Gates", "Warren Buffet"]
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
+t.write(names[0], font=(20))
+t.left(90)
+t.forward(50)
+t.write(names[1], font=(20))
+t.forward(50)
+t.write(names[2], font=(20))
+t.forward(50)
+t.write(names[3], font=(20))
