35256a960505db859500a62a2d8503ddf44f0828
Duo Chen <duochen@yahoo.com>
2019-07-15 20:59:47 +0200

    check in code

--- Lecture04_List_And_Loops/Exercises/greetings.py
+++ Lecture04_List_And_Loops/Exercises/greetings.py
@@ -1,6 +1,14 @@
+import turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
 names = ["Donald Trump", "Steve Jobs", "Bill Gates", "Warren Buffet"]
 message = " loves Python"
-print(names[0] + message)
-print(names[1] + message)
-print(names[2] + message)
-print(names[3] + message)
+t.write(names[0] + message, font=(20))
+t.left(90)
+t.forward(50)
+t.write(names[1] + message, font=(20))
+t.forward(50)
+t.write(names[2] + message, font=(20))
+t.forward(50)
+t.write(names[3] + message, font=(50))
---
+++ Lecture04_List_And_Loops/Exercises/greetings_old.py
@@ -0,0 +1,6 @@
+names = ["Donald Trump", "Steve Jobs", "Bill Gates", "Warren Buffet"]
+message = " loves Python"
+print(names[0] + message)
+print(names[1] + message)
+print(names[2] + message)
+print(names[3] + message)
