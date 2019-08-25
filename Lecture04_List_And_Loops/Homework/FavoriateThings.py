911d9b17b039b1fb058b95c266f41e7b5cd86482
Duo Chen <duochen@yahoo.com>
2019-08-24 23:41:46 -0500

    check in code

---
+++ Lecture04_List_And_Loops/Homework/FavoriateThings.py
@@ -0,0 +1,2 @@
+my_favoriate_things = ['Blue', 3, 'Desserts', 'Running', 33.3]
+print(f"These are Duo's favorite things: {my_favoriate_things}")
---
+++ Lecture04_List_And_Loops/Homework/RandomFactory.py
@@ -0,0 +1,5 @@
+random_items = ['basket', 'tennis', 'bread', 'table', 'ball', 'game', 'box']
+
+print(f"{random_items[1]} {random_items[4]}")
+
+print(f"{random_items[2]} {random_items[0]}")
